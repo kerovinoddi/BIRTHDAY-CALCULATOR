e8536bd06139d95fae1c2d5338e29ead161e6ddd
kerovinoddi <kerovineoddi@gmail.com>
2020-05-10 18:18:47 +0300

    updated js script

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function calculateDayValue() {
 
     d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
 
-    return (Math.floor(d));
+    return (Math.floor(d));>
 }
 function getGender() {
     var gendenders = document.getElementsByName("gender");
